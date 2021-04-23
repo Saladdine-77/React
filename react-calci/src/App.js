828c108f54dfe0fc32f36b6991bd69092f93d8f7
Saladdine <Saladdine@goog.com>
2021-04-23 06:52:02 -0700

    initial

---
+++ react-calci/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Main from './Main.js';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+       
+       <Main/>
+
+    </div>
+  );
+}
+
+export default App;
