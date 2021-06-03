49a4cc80ff6347de34478476521f24bdd283523c
Saladdine-77 <82659633+Saladdine-77@users.noreply.github.com>
2021-06-04 00:12:17 +0300

    My to do list app
    adding and deleting tasks from a list

---
+++ todo-list/todo-list/src/Main.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {useState} from 'react';
+
+
+export default function Main() {
+    const [task, setTask] = useState('');
+    const [taskList, setTaskList] = useState([]);
+
+    function addToDo(){
+        setTaskList([...taskList, task])
+
+    }
+    const taskListContent = taskList.map((task, index)=>{
+        return(
+            <div>
+                <p>{task}</p>
+                <button onClick= {()=>deleteTask(index)}>delete</button>
+                <i class="far fa-trash-alt" onClick= {()=>deleteTask(index)}></i>
+            </div>
+        )
+    })
+    function deleteTask(index){
+        alert(index)
+
+        var duparray = [...taskList]
+        duparray.splice(index,1)
+        setTaskList(duparray)
+    }
+    return (
+        <div>
+            <div className="row justify-content-center">
+                <div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
+                    <h1>Todo List React</h1>
+                    <input type="text" placeholder="Enter the task" className="form-control" value={task}
+                    onChange={(e)=>{setTask(e.target.value)}}
+                    />
+                    <button className="btn btn-success" onClick={addToDo}>Add</button>
+
+                    <br/>
+                    {taskListContent}
+                </div>
+            </div>
+        </div>
+    )
+}
